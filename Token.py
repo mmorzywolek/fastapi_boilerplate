a7720015806e3dcb648adc25456397460553c96f
maciek <maciej.morzywolek@gmail.com>
2022-07-25 10:46:13 +0200

    .gitignore change

--- Token.py
+++ Token.py
@@ -1,11 +1,15 @@
+import os
 from datetime import datetime, timedelta
 from jose import jwt, JWTError
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi import Depends, HTTPException, status
 from Schemas import TokenData
+from dotenv import load_dotenv
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = os.getenv('ALGORITHM')
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
